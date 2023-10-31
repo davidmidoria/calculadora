4aadd2a4f85bbe9b2b952ade3415ce4c0c146311
David williams pyrrho de lima <78514902+davidmidoria@users.noreply.github.com>
2023-10-31 13:15:08 -0300

    Adicionar arquivos de projeto.

---
+++ calculadora/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace calculadora {
+    class Program {
+        static double soma(double[] uni) {
+            double resultado=0;
+             foreach (var i in uni) {
+                resultado += i ;}
+             return resultado;
+        }
+        static double subtracao(double[] uni) {
+            double numeros =0;
+            foreach (var i in uni) {
+                numeros -= i ;
+            }
+            return numeros;
+        }
+        static double mult(double[] uni) {
+            double numeros = 1;
+           foreach (var i in uni) {
+                numeros *= i ;
+            }
+            return numeros;
+        }
+
+        
+        static void Main(string[] args) {
+            double[] numeros = { 1, 2, 3, 4, 5 };
+            Console.WriteLine(soma(numeros));
+        }
+    }
+}
